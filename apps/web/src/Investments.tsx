1a69617a73e4da832c36456cb6d0ee67cc2fcb9d
Munil123 <141709140+Munil123@users.noreply.github.com>
2025-08-15 15:43:38 +0530

    Create Investments.tsx

---
+++ apps/web/src/Investments.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Investments: React.FC = () => {
+  return (
+    <div className="investments">
+      <h1>Investments</h1>
+      <p>Investments page - TODO: Implement investment portfolio functionality</p>
+    </div>
+  );
+};
+
+export default Investments;
