fffd676f2ffeedb3c0ad918f91f739ff4512675f
Munil123 <141709140+Munil123@users.noreply.github.com>
2025-08-15 15:43:06 +0530

    Create Transactions.tsx

---
+++ apps/web/src/Transactions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Transactions: React.FC = () => {
+  return (
+    <div className="transactions">
+      <h1>Transactions</h1>
+      <p>Transactions page - TODO: Implement transaction management functionality</p>
+    </div>
+  );
+};
+
+export default Transactions;
