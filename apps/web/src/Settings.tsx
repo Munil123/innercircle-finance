3d4719ba68c1d583f9e318fdc87a9e5f6f79063f
Munil123 <141709140+Munil123@users.noreply.github.com>
2025-08-15 15:45:23 +0530

    Create Settings.tsx

---
+++ apps/web/src/Settings.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings: React.FC = () => {
+  return (
+    <div className="settings">
+      <h1>Settings</h1>
+      <p>Settings page - TODO: Implement user settings and preferences functionality</p>
+    </div>
+  );
+};
+
+export default Settings;
