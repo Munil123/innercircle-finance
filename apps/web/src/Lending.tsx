37f5a76e4bc2b422740bf1129063351c705142f1
Munil123 <141709140+Munil123@users.noreply.github.com>
2025-08-15 15:44:11 +0530

    Create Lending.tsx

---
+++ apps/web/src/Lending.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Lending: React.FC = () => {
+  return (
+    <div className="lending">
+      <h1>Lending</h1>
+      <p>Lending page - TODO: Implement lending and borrowing functionality</p>
+    </div>
+  );
+};
+
+export default Lending;
